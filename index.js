5691e524616f57c6109d30a99e77a0f9f8ca07cc
ada lovelace <williambookman@pursuit.org>
2023-12-04 17:16:58 -0500

    attempting to fix merge conflicts contiues

--- index.js
+++ index.js
@@ -8,7 +8,6 @@ const morseCodeDictionary = require("./morse-code-dictionary.json");
 function sortByStringLength(words) {
   // Use the sort() method with a custom comparison function
   words.sort(function(a, b) {
-<<<<<<< HEAD
   // Compare the lengths of the two words
   return a.length - b.length;
 });
@@ -17,30 +16,17 @@ return words;
 
 }
 
-=======
-    // Compare the lengths of the two words
-    return a.length - b.length;
-  });
-
-  return words;
-}
-
-// Example usage:
-const words = ["apple", "banana", "cherry", "date", "elderberry"];
-const sortedWords = sortByStringLength(words);
-console.log(sortedWords);
-// Question 1 was completed by Luis
->>>>>>> 56e52797c08b300b221374028f332f2388251c19
 
 
 /**
  * Returns an array of the word in all scrolling positions.
  * @param {String} word - A string.
  * @returns {string[]} An array of strings
+ * Example: "Hello"
+ * [ 'elloH', 'lloHe', 'loHel', 'oHell', 'Hello' ]
  */
 function textScroller(word) {
   const result = [];
-<<<<<<< HEAD
 
   // Loop through each character position
   for (let i = 0; i < word.length; i++) {
@@ -50,37 +36,45 @@ function textScroller(word) {
   // Push the rotated word to the result array
   result.push(rotatedWord);
 
-=======
-  
-  // Loop through each character position
-  for (let i = 0; i < word.length; i++) {
-    // Create a rotated version of the word
-    const rotatedWord = word.slice(i) + word.slice(0, i);
-    
-    // Push the rotated word to the result array
-    result.push(rotatedWord);
->>>>>>> 56e52797c08b300b221374028f332f2388251c19
   }
 
   return result;
 }
-<<<<<<< HEAD
-=======
-
-// Example usage:
-const word = "Brooklyn";
-const scrollingPositions = textScroller(word);
-console.log(scrollingPositions);
-
-// Question 2 was completed by Luis
->>>>>>> 56e52797c08b300b221374028f332f2388251c19
 
 /**
  * Returns the difference between the largest and smallest number in the array
  * @param {Number[]} numbers - An array of numbers.
  * @returns {Number} The difference between the largest and smallest number.
  */
-function betweenExtremes() {}
+function betweenExtremes(numbers) { 
+  // checks to see if the array only has numbers
+  const onlyNumbersCheck = numbers.every((element => typeof element === "number"));
+
+  //filters the array to only contain numbers
+  const onlyNumbersFilter = numbers.filter((element) => typeof element === "number") 
+
+  // stores the largest number in the array
+  const maxNumber = Math.max(...onlyNumbersFilter)
+
+  // stores the smallest number in the array
+  const minNumber = Math.min(...onlyNumbersFilter)
+
+  // stores the difference between the largest and smallest number in this array
+  const difference = maxNumber - minNumber
+
+  // return the original array if all elements are not all numbers
+  if (!onlyNumbersCheck) {
+  return numbers
+  }
+
+  // return empty array if input an empty string
+  if (numbers === " "){
+    return []
+  }
+  
+  // returns the difference
+  return difference 
+}
 
 /**
  * Returns the translation of English to morse code.
@@ -91,7 +85,27 @@ function betweenExtremes() {}
  * .- / -. . .-- / -- --- -. - ....
  * Hint: Check the morse-code-dictionary.json file to see what data is available.
  */
-function morseCodeTranslator() {}
+function morseCodeTranslator(message, dictionary) {
+  // uppercase every letter in the string to match with the dictionary
+  let uppercaseMessage = message.toUpperCase();
+
+  //initialize a variable to store the message translated in morse code
+  let morseCode = "";
+
+  // loops through the message, finds matches in the dictionary, and stores it in the variable morseCodeCharacter
+  for (const character of uppercaseMessage){
+    const morseCodeCharacter = Object.keys(dictionary).find(key => key === character);
+  
+  // if there is a match adds each character into the morseCode variable
+    if (morseCodeCharacter){
+      morseCode += dictionary[morseCodeCharacter] + " "
+
+  }
+}
+// return the final morse code message and trim any extra space at the end of the string
+return morseCode.trim();
+  
+}
 
 module.exports = {
   sortByStringLength,
